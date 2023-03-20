4577a740dbbbe28f747491cd2dc4c1694a97ea99
Gavdra321 <128329937+Gavdra321@users.noreply.github.com>
2023-03-19 20:45:31 -0400

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Amazon X-Ray Auto Display
+I put this together quickly because there is no way to see when new amazon xray trivia shows up.
+Unpacked chrome extension, does nothing except check for new trivia and display the list
+
+## Setup
+- Download code to local folder
+- Go to chrome://extensions/
+- Click "Load Unpacked"
+- Select Local Folder with code
+- Enable Extension
+- THATS IT
+- Next time you watch a movie that has amazon xray enjoy seeing the trivia
---
+++ content.js
@@ -0,0 +1,38 @@
+var nameList = [];
+function start() {
+    //setTimeout will check list every 5 seconds
+    setTimeout(function() {
+        console.log("running...")
+        //get all xray items without alerted class
+        const xrayList = document.querySelectorAll(".xrayQuickViewListItem:not(.alerted)");
+        xrayList.forEach(e=>{
+            //Check if people should be alerted based on name list
+            if (e.classList.contains("portraitImage")){
+                let currName = e.querySelector('.primaryText').innerHTML;
+                if (!nameList.includes(currName)){
+                    nameList.push(currName);
+                    //console.log("show b/c",e)
+                    showXRay()
+                } 
+            }
+            else{
+                //not a person just show xray and add alerted class
+                e.classList.add("alerted")
+                console.log("show b/c",e)
+                showXRay()
+            }
+        });
+        start();
+    },  5000);
+}
+
+function showXRay(){
+    const xray = document.getElementsByClassName("xrayQuickView")[0];
+    if (!xray.classList.contains("show")) xray.classList.add("show");
+    //show xray for 5 seconds
+    setTimeout(()=>{
+        xray.classList.remove("show");
+    },5000);
+}
+//run it
+start();
