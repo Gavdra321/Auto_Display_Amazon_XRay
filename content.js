3ac72670c2add4556cdc812f86634272675edbe8
Gavdra321 <128329937+Gavdra321@users.noreply.github.com>
2023-03-19 21:39:36 -0400

    Display for 1 second instead of 5

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ var nameList = [];
 function start() {
     //setTimeout will check list every 5 seconds
     setTimeout(function() {
-        console.log("running...")
+        //console.log("running...")
         //get all xray items without alerted class
         const xrayList = document.querySelectorAll(".xrayQuickViewListItem:not(.alerted)");
         xrayList.forEach(e=>{
@@ -18,7 +18,7 @@ function start() {
             else{
                 //not a person just show xray and add alerted class
                 e.classList.add("alerted")
-                console.log("show b/c",e)
+                //console.log("show b/c",e)
                 showXRay()
             }
         });
@@ -29,10 +29,10 @@ function start() {
 function showXRay(){
     const xray = document.getElementsByClassName("xrayQuickView")[0];
     if (!xray.classList.contains("show")) xray.classList.add("show");
-    //show xray for 5 seconds
+    //show xray for 1 second
     setTimeout(()=>{
         xray.classList.remove("show");
-    },5000);
+    },1000);
 }
 //run it
 start();
